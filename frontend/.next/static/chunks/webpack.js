041305121a7beab5a50c28eb74fc48d1f15ae49e
al-furkan <furkanf4336@gmail.com>
2025-12-01 23:18:34 +0600

    update code

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "78f28bdf94705042"; }
+/******/ 		__webpack_require__.h = function() { return "8714bda258fb12a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
